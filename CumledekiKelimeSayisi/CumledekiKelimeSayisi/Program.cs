da5c522fb00b6c574ca4e8310e01c90ebebdbef0
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-11 16:01:55 +0300

    CumledekiKelimeSayisi

---
+++ CumledekiKelimeSayisi/CumledekiKelimeSayisi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CumledekiKelimeSayisi
+{
+    internal class Program
+    {
+        private static object kelimeler;
+
+        static void Main(string[] args)
+        {
+            string metin;
+            Console.Write("Metni Girin  : ");
+            metin = Console.ReadLine();
+            string[] kelimeler = metin.Split(' ');
+            Console.WriteLine("Kelime Sayısı :" + kelimeler.Length);
+            foreach (string kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
